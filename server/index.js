e997c7b52ac57ab0502158dc4d680e0f0442e053
unknown <nicolabisc058@gmail.com>
2024-09-23 20:10:14 +0200

    upload

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on PORT ${process.env.PORT}.`)
 })
 
+
 const io = socket(server, {
   cors: {
     origin: 'http://localhost:3000',
